03fca3fc9b34453ea6481ad0d45ee87dac23a72e
Samuel <doglaszemo@gmail.com>
2016-08-07 15:43:54 -0400

    +java Script added text++++

--- Car.js
+++ Car.js
@@ -1,7 +1,7 @@
 /**
  * Created by Dinish on 8/7/2016.
  */
-console.log("The Sum of two Numbers");
+console.log("The Sum of two Numbers :-");
 function make(num1, num2){
     var total=num1+num2;
     return total;
